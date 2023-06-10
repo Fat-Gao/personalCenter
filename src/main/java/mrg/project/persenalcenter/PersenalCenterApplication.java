384a3ad29c73ab0caf1f096424778ce7df258a85
1952396 <312464740@qq.com>
2023-06-10 19:33:31 +0800

    Initial commit

---
+++ src/main/java/mrg/project/persenalcenter/PersenalCenterApplication.java
@@ -0,0 +1,13 @@
+package mrg.project.persenalcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersenalCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersenalCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mrg/project/persenalcenter/PersenalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package mrg.project.persenalcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersenalCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
